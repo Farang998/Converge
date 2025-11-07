bec88aa39dc84d54bad9ba4b4cdd2cccb87264eb
Chaitreee <202301243@daiict.ac.in>
2025-11-08 02:18:15 +0530

    Created middleware.py and edited asgi.py

--- backend/remote-work/mysite/asgi.py
+++ backend/remote-work/mysite/asgi.py
@@ -1,8 +1,8 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 import Chat.routing 
+from Chat.middleware import JWTAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
@@ -10,7 +10,7 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,  
-    "websocket": AuthMiddlewareStack(
+    "websocket": JWTAuthMiddleware(
         URLRouter(
             Chat.routing.websocket_urlpatterns  
         )
