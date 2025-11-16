bb54b6f166e6214321d1ef6a30ad49d5695dd617
Farang998 <160603714+Farang998@users.noreply.github.com>
2025-11-16 11:10:25 +0530

    Update urls.py for merging with main

--- backend/remote-work/api/projects/urls.py
+++ backend/remote-work/api/projects/urls.py
@@ -7,6 +7,8 @@ router = DefaultRouter()
 router.register(r'', ProjectViewSet, basename='project')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('create/', ProjectCreate.as_view(), name='project_create'),
     path('accept-invitation/<str:project_id>/', AcceptInvitation.as_view(), name='accept_invitation'),
+    path('userSearch/', searchuser.as_view(), name='user_search'),
+    path('', include(router.urls)),
 ]
